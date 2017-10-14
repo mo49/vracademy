d9e8446a41e0e90d556846ccc902e5f89fc5a2ea
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2017-10-14 21:24:21 +0900

    100ptごとにspown率を更新

---
+++ 20171014/20171014_shooting_game/Assets/_Complete-Game/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        public static int score;        // The player's score.
+        private EnemyManager[] scripts;
+        private int nextTargetScore = 100;
+
+        Text text;                      // Reference to the Text component.
+
+
+        void Awake ()
+        {
+            // Set up the reference.
+            text = GetComponent <Text> ();
+
+            // Reset the score.
+            score = 0;
+
+            scripts = GameObject.Find("EnemyManager").GetComponents<EnemyManager>();
+        }
+
+
+        void Update()
+        {
+            // Set the displayed text to be the word "Score" followed by the score value.
+            text.text = "Score: " + score;
+
+            if (score >= 30)
+            {
+                // 黄色
+                text.color = Color.yellow;
+            }
+            else if (score >= 20)
+            {
+                // 緑
+                text.color = Color.green;
+            }
+            else if (score >= 10)
+            {
+                // 青
+                text.color = Color.blue;
+            }
+            else
+            {
+                // 白
+                text.color = Color.white;
+            }
+
+            // 100ptごとにspown率を更新
+            if(score >= nextTargetScore){
+                nextTargetScore += 100;
+                foreach (var script in scripts)
+                {
+                    script.UpdateSpown();
+                }
+            }
+
+
+        }
+    }
+}
