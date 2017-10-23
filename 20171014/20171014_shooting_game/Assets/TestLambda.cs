eaf2d58425114f0d2450bf40ddbeaa1b2def24f8
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2017-10-23 14:18:16 +0900

    ラムダとLINQのサンプル追加

--- 20171014/20171014_shooting_game/Assets/TestLambda.cs
+++ 20171014/20171014_shooting_game/Assets/TestLambda.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using System.Linq;
 
 public class TestLambda : MonoBehaviour {
@@ -8,12 +9,48 @@ public class TestLambda : MonoBehaviour {
     IEnumerable<int> scores = Enumerable.Range(1, 10);
 
     void Start () {
+
+        // -------------------------------------------
+        // ラムダ式
+        // -------------------------------------------
+
+        // 戻り値あり
+        Func<int, int> mul = x => x * x;
+        Debug.Log("[Func] mul: " + mul(5));
+
+        // 戻り値なし
+        Action<int, int> sum = (x, y) => {
+            int t = x + y;
+            Debug.Log("[Action] sum: " + t);
+        };
+        sum(2, 3);
+
+        // デリゲート
+        Action<int, int> delegateSum = Sum;
+        delegateSum(2,3);
+
+
+        // -------------------------------------------
+        // LINQ
+        // -------------------------------------------
+
         var oddNums = scores
             .Where(score => score % 2 != 0)
             .OrderByDescending(score => score)
             .Take(3);
 
-        oddNums.ToList().ForEach(num => Debug.Log("[top3]奇数 => " + num));
+        oddNums.ToList().ForEach(num => Debug.Log("上位3つの奇数 => " + num));
+
+        var maxOddNum1 = scores
+            .Last(score => score % 2 != 0);
+
+        Debug.Log("[pattern1]最大の奇数 => " + maxOddNum1);
+
+        var maxOddNum2 = scores
+            .Where(score => score % 2 != 0)
+            .Max();
+
+        Debug.Log("[pattern2]最大の奇数 => " + maxOddNum2);
 
         var filteredNums1 = scores
             .Where((score, index) => index != 0 && index != scores.Count() - 1)
@@ -30,6 +67,11 @@ public class TestLambda : MonoBehaviour {
 
         filteredNums2.ToList().ForEach(num => Debug.Log("[pattern2]最初と最後を除く数を10で割る => " + num));
 
+    }
+
+    void Sum(int x, int y) {
+        int t = x + y;
+        Debug.Log("[Delegate] sum: " + t);
     }
 
 }
